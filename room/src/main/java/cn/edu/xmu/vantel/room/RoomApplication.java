c7b58c29073fdc55292697645daf1edfd4b748cf
Bwly999 <438225686@qq.com>
2022-07-18 14:42:06 +0800

    chore: fix pom

--- room/src/main/java/cn/edu/xmu/vantel/room/RoomApplication.java
+++ room/src/main/java/cn/edu/xmu/vantel/room/RoomApplication.java
@@ -1,4 +1,17 @@
 package cn.edu.xmu.vantel.room;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@MapperScan("cn.edu.xmu.vantel.room.mapper")
 public class RoomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoomApplication.class, args);
+    }
 }
